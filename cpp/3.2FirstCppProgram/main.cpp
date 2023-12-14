e6ece39761a9c99109712c6b57fcaf00f96f6602
Mouse <discomouse.wyeoo@simplelogin.com>
2023-12-14 08:26:45 +0000

    add extra lines for fun

--- cpp/3.2FirstCppProgram/main.cpp
+++ cpp/3.2FirstCppProgram/main.cpp
@@ -5,6 +5,9 @@ int main(){
     std::cout << "Number1" << std::endl;
     std::cout << "Number2" << std::endl;
     std::cout << "Number3" << std::endl;
+    std::cout << "Number4" << std::endl;
+    std::cout << "Number5" << std::endl;
+    std::cout << "Number6" << std::endl;
 
     return 0;
 }
